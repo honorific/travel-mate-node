f9d9f53c08a561b5ef31fa01b335e76eea7be415
honorific <honorific.info@gmail.com>
2023-03-17 19:47:01 +0330

    fix user model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema({
-  name: {type: String, min: 3, max: 50, required},
-  email: {type: String, min: 5, max: 50, required},
+  name: {type: String, min: 3, max: 50, required: true},
+  email: {type: String, min: 5, max: 50, required: true, unique: true},
   password: {type: String, required},
   photoURL: {type: String, default: ''},
 })
