1cfeb7b05526be7deca8867157095fe44bc956ca
honorific <honorific.info@gmail.com>
2023-03-30 12:52:25 +0430

    createRoom controller

--- controllers/room.js
+++ controllers/room.js
@@ -1,9 +1,8 @@
-export const createRoom = async (req, res) => {
-  res.status(201).json({
-    success: true,
-    result: {
-      id: 123,
-      title: 'test room',
-    },
-  })
-}
+import tryCatch from './utils/tryCatch.js'
+
+export const createRoom = tryCatch(async (req, res) => {
+  const {id: uid, name: uName, photoURL: uPhoto} = req.user
+  newRoom = new Room({...req.body, uid, uName, uPhoto})
+  await newRoom.save()
+  res.status(201).json({status: true, result: newRoom})
+})
